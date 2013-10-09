5c9443a08095cd256a839ae1b85591c337489a78
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-10-09 18:15:25 +0300

    dir walker added

--- twitnorm/languagefiltering.py
+++ twitnorm/languagefiltering.py
@@ -76,3 +76,20 @@ def usage():
     print 'python script.py [--language="en"] [--input_file="/home/cagil/Datasets/snap/snap.sample"] [--output_file="output_en.txt"]'
 if __name__ == "__main__":
     main()    
+
+import os
+def gen_walk(path='.'):
+    for dirname, dirnames, filenames in os.walk(path):
+        # print path to all subdirectories first.
+        for subdirname in dirnames:
+            print os.path.join(dirname, subdirname)
+
+        # print path to all filenames.
+        for filename in filenames:
+            yield os.path.join(dirname, filename)
+
+        # Advanced usage:
+        # editing the 'dirnames' list will stop os.walk() from recursing into there.
+        if '.git' in dirnames:
+            # don't go into any .git directories.
+            dirnames.remove('.git')
