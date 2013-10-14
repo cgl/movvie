f8e9b6c319868524ae9080e2b522fae1738b24ab
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-10-14 13:54:55 +0300

    graphlib under cons

--- twitnorm/graphlib.py
+++ twitnorm/graphlib.py
@@ -13,9 +13,19 @@
 import langid, getopt, sys, logging, os, socket
 import CMUTweetTagger
 import networkx as nx
+import enchant
 class Reader():
-    def __init__(format):
-        format=format
+    def __init__(path='None',format='None'):
+        this.format=format
+        this.path=path
+
+    # Returns lot
+    def read(file=None):
+        if path:
+            pass
+        elif file:
+            with open(infile) as l: 
+                yield l[2]
     
 from graph_tool.all import *
 class WordGraph():
@@ -41,7 +51,9 @@ class WordGraph():
         return g2
 
 class Tweet:
-    def __init()__:
+    d = enchant.Dict("en_US")
+    def __init(g)__:
+        this.graph = g
         pass
     
     def getTweets(tweets):
@@ -53,120 +65,33 @@ class Tweet:
 
     def getTweet(tweet):
         #tweet = [('example', 'N', 0.979), ('tweet', 'V', 0.7763), ('1', '$', 0.9916)]
+        words = []
         for w,t,c in tweet:
+            if ! this.graph.has_node(w):
+                this.graph.add_node(w,ovv=False if d.check(w) else True)
+                this.graph[w][t]=1
+            else:
+                if(this.graph[w].has_key(t)):
+                    this.graph[w][t] +=1
+                else:
+                    this.graph[w][t] =1
+            for x in words:
+                if this.graph.has_edge(w,x):
+                    # we added this one before, just increase the weight by one
+                    G[w][x]['weight'] += 1
+                else:
+                    # new edge. add with weight=1
+                    G.add_edge(subject_id, object_id, weight=1)
+            words.append(w)
+            
+            
+
+    def addNode(w):
+        if(G.node.has_key(w)):
+            return G.node
+                
+            
+            
             
             
         
-d = {'host': socket.gethostname(),}
-FORMAT = '%(asctime)-15s %(host)s -8s %(message)s'
-logging.basicConfig(format=FORMAT,filename='filtering.log',level=logging.DEBUG)
-
-def read_tweets(lang='en',infile='snap.sample'):
-    with open(infile) as f:        
-        A = None
-        U = None
-        T = None
-        english = None
-        for line in f:
-            if line.split('\t')[0] == 'T':
-                T = line.split('\t')[1].strip('\n')
-            elif line.split('\t')[0] == 'U':
-                U = line.split('\t')[1].strip('\n')
-            elif line.split('\t')[0] == 'W':
-                W = line.split('\t')[1].strip('\n')
-                if not (W is None) | (W == 'No Post Title'):
-                    if langid.classify(W)[0] == lang:
-                        english = True
-            elif english:
-                yield T,U,W
-                english=False
-
-def write_tweets(lang='en',infile='snap.sample',outfile='out.txt'):
-    logger = logging.getLogger('tcpserver')
-    logger.info('Started Processing : %s', infile , extra=d)
-    try:
-        tweets = read_tweets(lang=lang,infile=infile)
-        f = open(outfile,"w")
-        for chunk in tweets:
-            f.write(str(chunk)+'\n')
-        f.close()
-    except Exception, e:
-        logger.error(str(e),extra=d)
-        return
-    logger.info('End Processing : %s', infile , extra=d)
-    
-
-def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hliop:v", ["help", "language=", "input_file=", "output_file=","path="])
-    except getopt.GetoptError, err:
-        print str(err) # will print an error about option not recognized..
-        sys.exit(1)
-    # Default option values
-    lang='en'
-    infile='snap.sample'
-    outfile='out.txt'
-    verbose = False
-    # Process cmd line options
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit(2)
-        elif o in ("-l", "--language"):
-            lang = a
-        elif o in ("-i", "--input_file"):
-            infile = a
-        elif o in ("-o", "--output_file"):
-            outfile = a
-        elif o in ("-p", "--path"):
-            files = gen_walk(a)
-            for e,f in enumerate(files):
-                folder = '/'.join(f.split("/")[:-1])
-                os.system("mkdir -p out/%s" %folder )
-                write_tweets(lang=lang,infile=f,outfile='out/%s' % (f))
-            return
-        else:
-            assert False, "unhandled option!"    
-    write_tweets(lang=lang,infile=infile,outfile=outfile)
-
-def gen_walk(path='.'):
-    for dirname, dirnames, filenames in os.walk(path):
-        # print path to all subdirectories first.
-        #for subdirname in dirnames:
-        #            print os.path.join(dirname, subdirname)
-
-        # print path to all filenames.
-        for filename in filenames:
-            yield os.path.join(dirname, filename)
-
-        # Advanced usage:
-        # editing the 'dirnames' list will stop os.walk() from recursing into there.
-        if '.git' in dirnames:
-            # don't go into any .git directories.
-            dirnames.remove('.git')
-
-def usage():
-    print 'python script.py [--language="en"] [--input_file="/home/cagil/Datasets/snap/snap.sample"] [--output_file="output_en.txt"]'
-    print 'ex: python languagefiltering.py --language="en" --input_file="/home/cagil/Datasets/snap/tweets2009-07.txt" --output_file="output_en_07.txt"'
-    print 'ex: python languagefiltering.py --path=test2/'
-if __name__ == "__main__":
-    main()    
-
-import os
-def gen_walk(path='.'):
-    for dirname, dirnames, filenames in os.walk(path):
-        # print path to all subdirectories first.
-        for subdirname in dirnames:
-            print os.path.join(dirname, subdirname)
-
-        # print path to all filenames.
-        for filename in filenames:
-            yield os.path.join(dirname, filename)
-
-        # Advanced usage:
-        # editing the 'dirnames' list will stop os.walk() from recursing into there.
-        if '.git' in dirnames:
-            # don't go into any .git directories.
-            dirnames.remove('.git')
