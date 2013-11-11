91978488ba8983d78b1328e00aa4641ab960867f
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-11-11 12:52:05 +0200

    standalone workable

--- twitnorm/TwPipe/cleanDb.py
+++ twitnorm/TwPipe/cleanDb.py
@@ -10,12 +10,12 @@ def update_edges_tag(database='tweets_current'):
 
     lot = CMUTweetTagger.runtagger_parse(tweets)
 
-    N = normalizer.Normalizer(lot,database)
-    tags = N.nodes.distinct('tag')
+    norm = normalizer.Normalizer(lot,database)
+    tags = norm.nodes.distinct('tag')
     for tag in tags:
-        nouns = [node['_id'] for node in filter(lambda x: x['freq']> 8, N.nodes.find({'tag':tag}))]
-        N.edges.update({'from': { '$in' : nouns}},{'$set' : {u'from_tag':tag } },multi=True)
-        N.edges.update({'to': { '$in' : nouns}},{'$set' : {u'to_tag':tag } },multi=True)
+        nouns = [node['_id'] for node in filter(lambda x: x['freq']> 8, norm.nodes.find({'tag':tag}))]
+        norm.edges.update({'from': { '$in' : nouns}},{'$set' : {u'from_tag':tag } },multi=True)
+        norm.edges.update({'to': { '$in' : nouns}},{'$set' : {u'to_tag':tag } },multi=True)
 
 def ensure_indexes(database='tweets_current'):
     from pymongo import MongoClient
@@ -25,3 +25,6 @@ def ensure_indexes(database='tweets_current'):
     db.edges.ensure_index([ ('from', 1), ('to', 1) , ('dis', 1 )] )
     db.edges.ensure_index('from_tag', 1)
     db.edges.ensure_index('to_tag', 1)
+
+if __name__ == "__main__":
+    update_edges_tag()
