c99a80c0b5b062deb825a305830c8301b6870a09
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-11-15 18:02:27 +0200

    exception added v5

--- twitnorm/TwPipe/analysis.py
+++ twitnorm/TwPipe/analysis.py
@@ -78,8 +78,9 @@ def get_score_line(cand,sumof,ovv,ovv_snd,suggestions):
     try:
         lev = Levenshtein.distance(unicode(ovv_snd),soundex.soundex(cand.decode("utf-8","ignore")))
     except UnicodeEncodeError:
-        print 'UnicodeEncodeError[ovv_snd]: %s %s' % (ovv_snd.encode('utf-8',"ignore"),cand.decode("utf-8","ignore"))
-        lev = Levenshtein.distance(ovv_snd.encode('utf-8',"ignore"),soundex.soundex(cand))
+        print ovv_snd
+        print 'UnicodeEncodeError[ovv_snd]: %s %s' % (ovv_snd.encode('ascii',"ignore"),cand.decode("utf-8","ignore"))
+        lev = Levenshtein.distance(ovv_snd.encode('ascii',"ignore"),soundex.soundex(cand))
     except UnicodeDecodeError:
         print 'UnicodeDecodeError'
         lev = Levenshtein.distance(ovv_snd,soundex.soundex(cand.decode("utf-8","ignore")))
