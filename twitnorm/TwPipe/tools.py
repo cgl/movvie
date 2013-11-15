3196f354c335736d247d3c9e1e553dce0fb53c5b
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-11-15 14:58:49 +0200

    top_n

--- twitnorm/TwPipe/tools.py
+++ twitnorm/TwPipe/tools.py
@@ -1,9 +1,28 @@
 import re
 import os
 import fuzzy
+import constants
 
-salient=0.00000000001
-
+def top_n(res,n=100):
+    in_top_n = 0
+    total_ill = 0
+    index_list = {}
+    for res_ind in range(0,len(res)):
+        answer = constants.mapping[res_ind][1]
+        ovv = constants.mapping[res_ind][0]
+        if answer != ovv:
+            total_ill += 1
+            if res[res_ind]:
+                res_list = [a[0] for a in res[res_ind][n]]
+                if answer in res_list:
+                    in_top_n += 1
+                    ind = res_list.index(answer)
+                    index_list_n = index_list.get(ind,[0,[]])
+                    index_list_n[0] += 1
+                    index_list_n[1].append(res_ind)
+                    index_list[ind] = index_list_n
+    print 'Out of %d, %d has an normalization, we^ve %d of those correct normalizations in our list with indexes %s' % (len(res),total_ill, in_top_n,[(a, index_list[a][0]) for a in index_list])
+    return index_list
 
 def isvalid(w):
     #return true if string contains any alphanumeric keywors
