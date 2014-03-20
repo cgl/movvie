ccc092ca9730010ef3e2d49a7f0527cbe035fb98
Çağıl Uluşahin <cagilulusahin@gmail.com>
2014-03-20 20:55:32 +0200

    yml

--- movvie/urls.py
+++ movvie/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'movvie.views.home', name='home'),
+    url(r'^$', 'movvie.views.home', name='home'),
     # url(r'^movvie/', include('movvie.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
