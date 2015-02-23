76790ceb4edd5534672c1cf4969e3b26b608fbf4
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-02-23 16:26:41 +0200

    v2.0

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import RedirectView
 
 from main import views
 
 urlpatterns = patterns('',
     # ex: /polls/
-    url(r'^$', views.all, name='all'),
+    url(r'movies/$', views.all, name='all'),
+    url(r'tweets/$', views.tweets, name='get_tweets'),
+    url(r'about/$', views.about, name='about'),
+    url(r'tweets/campaign/(?P<camp_id>\w+)/$', views.campaign.as_view() , name='campaign2', ),
+#    url(r'tweets/campaign/(?P<camp_id>\w+)/$', views.campaign_json, name='campaign_json'),
+
+    url(r'tweets/campaign/(?P<camp_id>\w+)/(?P<start>\d+)/(?P<limit>\d+)/$', views.campaign, name='campaign'),
+    url(r'tweets/campaign/(?P<camp_id>\w+)/json/(?P<start>\d+)/(?P<limit>\d+)/$', views.campaign_json, name='campaign_json'),
     # ex: /polls/5/
 #    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
--- main/views.py
+++ main/views.py
@@ -3,6 +3,11 @@ from main.models import Movie as Movvie ,Review
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
+import urllib2, json
+from django.views.decorators.cache import cache_page
+import sys
+
+import analysis
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
@@ -13,3 +18,44 @@ def all(request):
     return render_to_response('index.html',
                               {"movs": movies},
                                context_instance=RequestContext(request))
+def about(request):
+    return render_to_response('about.html',
+                              {},
+                               context_instance=RequestContext(request))
+
+@cache_page(60 * 45)
+def tweets(request):
+    print("tweetsssss")
+    response = urllib2.urlopen('http://direnaj-staging.cmpe.boun.edu.tr/campaigns/list?auth_user_id=direnaj&auth_password=tamtam')
+    #response = urllib2.urlopen('http://direnaj-staging.cmpe.boun.edu.tr/campaigns/filter?auth_user_id=direnaj&auth_password=tamtam&limit=10&skip=0')
+    camp_json = json.load(response)
+    campaigns =  [{'id':ins['campaign_id'], 'desc':ins['description'] if ins['description'] != "bos" else ins['query_terms'] } for ins in camp_json]
+    return render_to_response('tweets.html',
+                              {"camps": campaigns},
+                               context_instance=RequestContext(request))
+from django.utils import simplejson
+
+
+import sys ; sys.path.append("/home/cagil/repos/movvie/twitnorm/TwPipe/febrl-0.4.2/") ; sys.path.append("/usr/lib/python2.7/dist-packages/") ; sys.path.append("/usr/lib/pymodules/python2.7/")
+
+def campaign_json_inner(camp_id,start,limit):
+    response = urllib2.urlopen('http://direnaj-staging.cmpe.boun.edu.tr/statuses/filter?skip=0&auth_user_id=direnaj&auth_password=tamtam&campaign_id=%s&limit=%s&skip=%s' % (camp_id,limit,start))
+    camp_json = json.load(response)
+    print(analysis.normalize("hello I am hre"))
+    tweets =  [{'text' : tweet_json['tweet']['text'], 'normalized_text' : analysis.normalize(tweet_json['tweet']['text'])} for tweet_json in camp_json['results']]
+    return tweets
+@cache_page(60 * 4500)
+def campaign(request,camp_id,start,limit):
+    #response = urllib2.urlopen('http://direnaj-staging.cmpe.boun.edu.tr/statuses/filter?skip=0&auth_user_id=direnaj&auth_password=tamtam&campaign_id=%s&limit=100' % camp_id)
+    #response = urllib2.urlopen('http://direnaj-staging.cmpe.boun.edu.tr/campaigns/filter?auth_user_id=direnaj&auth_password=tamtam&limit=10&skip=0')
+    #camp_json = json.load(response)
+    tweets = campaign_json_inner(camp_id,start,limit)
+    return render_to_response('tweets.html',
+                              {"tweets": tweets},
+                               context_instance=RequestContext(request))
+
+@cache_page(60 * 4500)
+def campaign_json(request,camp_id,start,limit):
+    tweets = campaign_json_inner(camp_id,start,limit)
+    return HttpResponse(simplejson.dumps(tweets), mimetype='application/json')
+
